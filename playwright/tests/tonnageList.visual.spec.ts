01f33cc4e40407435017cce1cd66bc66e9ff456b
tklepacki <t.klepacki@thesignalgroup.com>
2026-02-09 19:38:41 +0100

    Adding Final Version

--- playwright/tests/tonnageList.visual.spec.ts
+++ playwright/tests/tonnageList.visual.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { debounceDom } from '../helpers/common';
+import tonnageList from '../fixtures/tonnageList.json';
+import tce from '../fixtures/tce.json';
 
 test('Signal Platform - Tonnage List', async ({ page }) => {
+    await page.route('/api/tonnageList?basisPortID**', async (route) => {
+        await route.fulfill({ json: tonnageList });
+    });
+    await page.route('/api/tonnageList/tce?tabId**', async (route) => {
+        await route.fulfill({ json: tce });
+    });
+
     await page.goto('/');
     await page.fill("#enterEmailFormEmail", "user1@stresstest.com");
     await page.click("#enterEmailFormSubmit");
@@ -18,5 +27,5 @@ test('Signal Platform - Tonnage List', async ({ page }) => {
     await page.waitForLoadState('networkidle')
     await page.waitForLoadState('domcontentloaded')
     await debounceDom(page)
-    await expect(page).toHaveScreenshot();
+    await expect(page).toHaveScreenshot(({ mask: [page.locator('.tw-flex-shrink-0'), page.locator('img.tw-h-4.tw-w-4')] }));
 });
